9f27bdff7e213ab4104866ab726bb9260399a6e6
Natashalucy141 <Natashalucy141@users.noreply.github.com>
2020-11-02 20:09:00 +0000

    Added GitHub repo Url and Linkedin URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Created by{" "}
+        <a
+          href="https://www.linkedin.com/in/natasha-aldridge-68a65ab7/"
+          target="_blank"
+        >
+          Natasha Aldridge
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/Natashalucy141/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
