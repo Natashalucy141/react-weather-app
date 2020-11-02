d483204b7a9a143dd627c2678bef186b398f60b1
Natashalucy141 <Natashalucy141@users.noreply.github.com>
2020-11-02 20:28:29 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        Created by{" "}
-        <a
-          href="https://www.linkedin.com/in/natasha-aldridge-68a65ab7/"
-          target="_blank"
-        >
-          Natasha Aldridge
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://github.com/Natashalucy141/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          Created by{" "}
+          <a
+            href="https://www.linkedin.com/in/natasha-aldridge-68a65ab7/"
+            target="_blank"
+          >
+            Natasha Aldridge
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://github.com/Natashalucy141/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
