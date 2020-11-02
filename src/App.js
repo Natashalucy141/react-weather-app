de2922594c2830fa84c6d21c07bb17c9c1be81ae
Natashalucy141 <Natashalucy141@users.noreply.github.com>
2020-11-02 21:09:46 +0000

    added basic html of Weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         Weather App
+        <Weather />
         <footer>
           Created by{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="Search"
+          placeholder="Enter a city"
+          className="Search-Bar"
+        ></input>
+        <input type="submit" Value="Go"></input>
+      </form>
+      <h1>LONDON</h1>
+      <ul>
+        <li>2 November 2020, 20:42</li>
+        <li>Partly cloudy</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">⛅️ 14 ℃</div>
+
+        <div className="col.6">
+          <ul>
+            <li>Change of rain 10 %</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 16 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
